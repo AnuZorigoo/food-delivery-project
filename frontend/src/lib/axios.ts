6fec38927bab197eae42d9d191dee6e918ff7c2c
Anu <25LP7596@nest.edu.mn>
2026-03-03 11:21:22 +0800

    backend

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "BaCKEND_URL",
+  baseURL: process.env.BACKEND_URL,
   headers: { "Content-Type": "application/json" },
 });
